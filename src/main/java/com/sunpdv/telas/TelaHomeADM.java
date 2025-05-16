737afdb0a56374e8c1f51de7ecd6e93554fa45e9
jpmendes2000 <jpmoraes.mendes22@gmail.com>
2025-05-16 10:47:42 -0300

    inicio tela home adm - erro

--- src/main/java/com/sunpdv/telas/TelaHomeADM.java
+++ src/main/java/com/sunpdv/telas/TelaHomeADM.java
@@ -1,5 +1,31 @@
 package com.sunpdv.telas;
 
-public class TelaHomeADM {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TelaHomeADM extends Application{
     
+    @Override 
+    public void start(Stage stage) {
+        
+        VBox root = new VBox(15);
+            root.setAlignment(Pos.CENTER);
+            root.setPadding(new Insets(20));
+            
+        Scene scene = new Scene(root, 540, 370);
+        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
+        stage.setScene(scene);
+        stage.setTitle("Login - SUN PDV");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
