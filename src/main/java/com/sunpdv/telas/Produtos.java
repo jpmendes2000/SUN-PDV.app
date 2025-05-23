09c11b25b8d47417e3662980eaf8e60054aeef14
Mendes <jpmendes@Mendes.ArchLinux>
2025-05-23 11:22:03 -0300

    organização, estilização tela login

--- src/main/java/com/sunpdv/telas/Produtos.java
+++ src/main/java/com/sunpdv/telas/Produtos.java
@@ -35,7 +35,7 @@ public class Produtos {
     private Connection getConnection() throws SQLException {
         String url = "jdbc:sqlserver://localhost:1433;databaseName=SUN_PDVlocal;encrypt=true;trustServerCertificate=true";
         String user = "sa";
-        String password = "Jp081007!";
+        String password = "Senha@1234";
         return DriverManager.getConnection(url, user, password);
     }
 
