958bdf478df1e0e4e7c6831b4eeae569e8cf19ed
JP-Schinato <jpsilvaschinato@gmail.com>
2025-10-14 23:33:52 -0300

    feat: launcher and .exe

---
+++ src/main/java/com/sunpdv/Launcher.java
@@ -0,0 +1,13 @@
+package com.sunpdv;
+
+import com.sunpdv.telas.operation.LoginApp;
+
+/**
+ * Classe Launcher para iniciar a aplicação JavaFX
+ * Esta classe é necessária para executar JavaFX a partir de um JAR
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        LoginApp.main(args);
+    }
+}
